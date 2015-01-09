57d9068c5b06353001738fca19f45d38c34e8a7f
Josh Shagam <josh.shagam@hbo.com>
2015-01-08 17:34:29 -0800

    Support multiple targets, add tarballification

--- build-targets.sh
+++ build-targets.sh
@@ -9,22 +9,10 @@ if [ -z "$ANDROID_NDK_ROOT" ] ; then
     exit 1
 fi
 
-ABI=${1:-armeabi-v7a}
-
-case "$ABI" in
-armeabi-v7a)
-    V8TARGET=android_arm.release
-    V8OPTS="arm7=true"
-    ;;
-arm)
-    V8TARGET=android_arm.release
-    V8OPTS="arm7=false"
-    ;;
-*)
-    echo "Unknown/unsupported ABI $ABI" 1>&2
-    exit 1
-    ;;
-esac
+ABILIST="$@"
+if [ -z "$ABILIST" ] ; then
+    ABILIST="armeabi-v7a"
+fi
 
 BUNDLE=out/hadron-v8-bundle/`git rev-parse --short HEAD`
 mkdir -p $BUNDLE
@@ -33,8 +21,28 @@ cp -r include $BUNDLE
 
 make builddeps || exit 1
 
-make clean
-echo "Outputting V8 build log to $BUILT/build.log"
-make $V8TARGET i18nsupport=off -j 6 $V8OPTS || exit 1
-mkdir $BUNDLE/$ABI/lib
-cp out/$V8TARGET/*.a $BUNDLE/$ABI/lib
+for ABI in $ABILIST ; do
+    ABI=${1:-armeabi-v7a}
+
+    case "$ABI" in
+    armeabi-v7a)
+        V8TARGET=android_arm.release
+        V8OPTS="arm7=true"
+        ;;
+    arm)
+        V8TARGET=android_arm.release
+        V8OPTS="arm7=false"
+        ;;
+    *)
+        echo "Unknown/unsupported ABI $ABI" 1>&2
+        exit 1
+        ;;
+    esac
+
+    make clean
+    make $V8TARGET i18nsupport=off -j 6 $V8OPTS || exit 1
+    mkdir -p $BUNDLE/$ABI/lib
+    cp out/$V8TARGET/*.a $BUNDLE/$ABI/lib
+done
+
+(cd out ; tar czvf hadron-v8-bundle.tar.gz hadron-v8-bundle )

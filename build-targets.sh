f3fe6ef6eca08a99565669d7810ab86f79c3dfca
Josh Shagam <josh.shagam@hbo.com>
2015-01-08 17:13:05 -0800

    Make it easy to package up our built targets

--- build-targets.sh
+++ build-targets.sh
@@ -4,22 +4,37 @@
 # ensure we run in this directory
 cd "`dirname $0`"
 
-# Only rebuild if we're at a different revision than what's been built already, unless 'force' is specified
-BUILDREV=out/buildRev
-if [ "$1" != "force" ] && [ -f $BUILDREV ] && [ "`cat $BUILDREV`" = "`git rev-parse HEAD`" ] ; then
-    echo "V8 is up-to-date"
-    exit 0
+if [ -z "$ANDROID_NDK_ROOT" ] ; then
+    echo "ANDROID_NDK_ROOT variable not set" 2>&1
+    exit 1
 fi
 
-if [ -z "$ANDROID_NDK_ROOT" ] ; then
-    echo "Android NDK not configured; skipping"
-    exit 0
-fi
+ABI=${1:-armeabi-v7a}
+
+case "$ABI" in
+armeabi-v7a)
+    V8TARGET=android_arm.release
+    V8OPTS="arm7=true"
+    ;;
+arm)
+    V8TARGET=android_arm.release
+    V8OPTS="arm7=false"
+    ;;
+*)
+    echo "Unknown/unsupported ABI $ABI" 1>&2
+    exit 1
+    ;;
+esac
+
+BUNDLE=out/hadron-v8-bundle/`git rev-parse --short HEAD`
+mkdir -p $BUNDLE
+
+cp -r include $BUNDLE
+
+make builddeps || exit 1
 
 make clean
-
-# builddeps needs manual intervention to accept some certs
-yes p | make builddeps || exit 1
-make android_arm.release -j6 i18nsupport=off || exit 1
-
-git rev-parse HEAD > $BUILDREV
+echo "Outputting V8 build log to $BUILT/build.log"
+make $V8TARGET i18nsupport=off -j 6 $V8OPTS || exit 1
+mkdir $BUNDLE/$ABI/lib
+cp out/$V8TARGET/*.a $BUNDLE/$ABI/lib
